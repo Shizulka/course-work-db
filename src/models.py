0916d6c87034eb38275d2150aeaabb97af37336b
Shizulka <yrina.lishuk2006@gmail.com>
2025-12-11 14:55:37 +0200

    Create  models.py

---
+++ src/models.py
@@ -0,0 +1,197 @@
+#(.venv) PS C:\Users\Home\PycharmProjects\fastApiProject> sqlacodegen postgresql://postgres:IAnAFRyJnFpWsmUGiVLdfohFPCedaXDN@hopper.proxy.rlwy.net:44865/railway
+#Ми використали цю команду в терміналі що б отримати на основі нашої бази данних гогтовий пайтон скрипт , можливо далі може його змінимо але як факт поки так
+
+from typing import Optional
+import datetime
+
+from sqlalchemy import Boolean, CheckConstraint, Column, DateTime, Enum, ForeignKeyConstraint, Index, Integer, PrimaryKeyConstraint, String, Table, Text, UniqueConstraint, text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Author(Base):
+    __tablename__ = 'author'
+    __table_args__ = (
+        PrimaryKeyConstraint('author_id', name='author_pkey'),
+    )
+
+    author_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    book: Mapped[list['Book']] = relationship('Book', secondary='author_book', back_populates='author')
+
+
+class Book(Base):
+    __tablename__ = 'book'
+    __table_args__ = (
+        CheckConstraint('pages > 0', name='book_pages_check'),
+        PrimaryKeyConstraint('book_id', name='book_pkey')
+    )
+
+    book_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    language: Mapped[str] = mapped_column(String(255), nullable=False)
+    publisher: Mapped[str] = mapped_column(String(50), nullable=False, server_default=text("'Невідомо'::character varying"))
+    year_published: Mapped[Optional[int]] = mapped_column(Integer)
+    pages: Mapped[Optional[int]] = mapped_column(Integer)
+
+    author: Mapped[list['Author']] = relationship('Author', secondary='author_book', back_populates='book')
+    genre: Mapped[list['Genre']] = relationship('Genre', secondary='book_genres', back_populates='book')
+    book_copy: Mapped[list['BookCopy']] = relationship('BookCopy', back_populates='book')
+    waitlist: Mapped[list['Waitlist']] = relationship('Waitlist', back_populates='book')
+    wishlist: Mapped[list['Wishlist']] = relationship('Wishlist', back_populates='book')
+
+
+class FlywaySchemaHistory(Base):
+    __tablename__ = 'flyway_schema_history'
+    __table_args__ = (
+        PrimaryKeyConstraint('installed_rank', name='flyway_schema_history_pk'),
+        Index('flyway_schema_history_s_idx', 'success')
+    )
+
+    installed_rank: Mapped[int] = mapped_column(Integer, primary_key=True)
+    description: Mapped[str] = mapped_column(String(200), nullable=False)
+    type: Mapped[str] = mapped_column(String(20), nullable=False)
+    script: Mapped[str] = mapped_column(String(1000), nullable=False)
+    installed_by: Mapped[str] = mapped_column(String(100), nullable=False)
+    installed_on: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False, server_default=text('now()'))
+    execution_time: Mapped[int] = mapped_column(Integer, nullable=False)
+    success: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    version: Mapped[Optional[str]] = mapped_column(String(50))
+    checksum: Mapped[Optional[int]] = mapped_column(Integer)
+
+
+class Genre(Base):
+    __tablename__ = 'genre'
+    __table_args__ = (
+        PrimaryKeyConstraint('genre_id', name='genre_pkey'),
+        UniqueConstraint('name', name='genre_name_key')
+    )
+
+    genre_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    book: Mapped[list['Book']] = relationship('Book', secondary='book_genres', back_populates='genre')
+
+
+class Patron(Base):
+    __tablename__ = 'patron'
+    __table_args__ = (
+        CheckConstraint("email::text ~* '^[A-Za-z0-9._+%%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'::text", name='email'),
+        CheckConstraint("length(phone_number::text) = 10 AND phone_number::text ~ '^[0-9]+$'::text", name='phone_number'),
+        PrimaryKeyConstraint('patron_id', name='patron_pkey'),
+        UniqueConstraint('email', name='patron_email_key')
+    )
+
+    patron_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), nullable=False)
+    phone_number: Mapped[Optional[str]] = mapped_column(String(50))
+
+    notification: Mapped[list['Notification']] = relationship('Notification', back_populates='patron')
+    waitlist: Mapped[list['Waitlist']] = relationship('Waitlist', back_populates='patron')
+    wishlist: Mapped[list['Wishlist']] = relationship('Wishlist', back_populates='patron')
+    checkout: Mapped[list['Checkout']] = relationship('Checkout', back_populates='patron')
+
+
+t_author_book = Table(
+    'author_book', Base.metadata,
+    Column('author_id', Integer),
+    Column('book_id', Integer),
+    ForeignKeyConstraint(['author_id'], ['author.author_id'], name='author_book_author_id_fkey'),
+    ForeignKeyConstraint(['book_id'], ['book.book_id'], name='author_book_book_id_fkey')
+)
+
+
+class BookCopy(Base):
+    __tablename__ = 'book_copy'
+    __table_args__ = (
+        ForeignKeyConstraint(['book_id'], ['book.book_id'], name='book_copy_book_id_fkey'),
+        PrimaryKeyConstraint('book_copy_id', name='book_copy_pkey')
+    )
+
+    book_copy_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    copy_number: Mapped[str] = mapped_column(String(255), nullable=False)
+    book_id: Mapped[Optional[int]] = mapped_column(Integer)
+
+    book: Mapped[Optional['Book']] = relationship('Book', back_populates='book_copy')
+    checkout: Mapped[list['Checkout']] = relationship('Checkout', back_populates='book_copy')
+
+
+t_book_genres = Table(
+    'book_genres', Base.metadata,
+    Column('book_id', Integer, primary_key=True),
+    Column('genre_id', Integer, primary_key=True),
+    ForeignKeyConstraint(['book_id'], ['book.book_id'], ondelete='CASCADE', name='book_genres_book_id_fkey'),
+    ForeignKeyConstraint(['genre_id'], ['genre.genre_id'], ondelete='CASCADE', name='book_genres_genre_id_fkey'),
+    PrimaryKeyConstraint('book_id', 'genre_id', name='book_genres_pkey')
+)
+
+
+class Notification(Base):
+    __tablename__ = 'notification'
+    __table_args__ = (
+        ForeignKeyConstraint(['patron_id'], ['patron.patron_id'], name='notification_patron_id_fkey'),
+        PrimaryKeyConstraint('notification_id', name='notification_pkey')
+    )
+
+    notification_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    contents: Mapped[str] = mapped_column(Text, nullable=False)
+    patron_id: Mapped[Optional[int]] = mapped_column(Integer)
+
+    patron: Mapped[Optional['Patron']] = relationship('Patron', back_populates='notification')
+
+
+class Waitlist(Base):
+    __tablename__ = 'waitlist'
+    __table_args__ = (
+        ForeignKeyConstraint(['book_id'], ['book.book_id'], name='waitlist_book_id_fkey'),
+        ForeignKeyConstraint(['patron_id'], ['patron.patron_id'], name='waitlist_patron_id_fkey'),
+        PrimaryKeyConstraint('waitlist_id', name='waitlist_pkey')
+    )
+
+    waitlist_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    patron_id: Mapped[Optional[int]] = mapped_column(Integer)
+    book_id: Mapped[Optional[int]] = mapped_column(Integer)
+
+    book: Mapped[Optional['Book']] = relationship('Book', back_populates='waitlist')
+    patron: Mapped[Optional['Patron']] = relationship('Patron', back_populates='waitlist')
+
+
+class Wishlist(Base):
+    __tablename__ = 'wishlist'
+    __table_args__ = (
+        ForeignKeyConstraint(['book_id'], ['book.book_id'], name='wishlist_book_id_fkey'),
+        ForeignKeyConstraint(['patron_id'], ['patron.patron_id'], name='wishlist_patron_id_fkey'),
+        PrimaryKeyConstraint('wishlist_id', name='wishlist_pkey')
+    )
+
+    wishlist_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    patron_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    book_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    added_date: Mapped[Optional[datetime.datetime]] = mapped_column(DateTime, server_default=text('CURRENT_TIMESTAMP'))
+
+    book: Mapped['Book'] = relationship('Book', back_populates='wishlist')
+    patron: Mapped['Patron'] = relationship('Patron', back_populates='wishlist')
+
+
+class Checkout(Base):
+    __tablename__ = 'checkout'
+    __table_args__ = (
+        ForeignKeyConstraint(['book_copy_id'], ['book_copy.book_copy_id'], name='checkout_book_copy_id_fkey'),
+        ForeignKeyConstraint(['patron_id'], ['patron.patron_id'], name='checkout_patron_id_fkey'),
+        PrimaryKeyConstraint('checkout_id', name='checkout_pkey')
+    )
+
+    checkout_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    start_time: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+    status: Mapped[str] = mapped_column(Enum('Overdue', 'Soon', 'OK', name='status_type'), nullable=False, server_default=text("'OK'::status_type"))
+    book_copy_id: Mapped[Optional[int]] = mapped_column(Integer)
+    patron_id: Mapped[Optional[int]] = mapped_column(Integer)
+    end_time: Mapped[Optional[datetime.datetime]] = mapped_column(DateTime)
+
+    book_copy: Mapped[Optional['BookCopy']] = relationship('BookCopy', back_populates='checkout')
+    patron: Mapped[Optional['Patron']] = relationship('Patron', back_populates='checkout')
