ca84806b9f53754e6b8b761066008d5c1faebf7b
kohanyuk-a <kohanyuk.anastasia@lll.kpi.ua>
2025-12-15 02:10:01 +0200

    Update checkout.py

--- src/routers/checkout.py
+++ src/routers/checkout.py
@@ -19,6 +19,10 @@ def get_checkout_service(db: Session = Depends(get_db)) -> CheckoutService:
 def return_book( patron_id :int , book_copy_id :int , service: CheckoutService = Depends(get_checkout_service)):
     return service.return_book(  patron_id=patron_id,  book_copy_id=book_copy_id)
 
+@router.post("/lost_book")
+def lost_book( patron_id :int , book_copy_id :int , service: CheckoutService = Depends(get_checkout_service)):
+    return service.lost_book(  patron_id=patron_id,  book_copy_id=book_copy_id)
+
 @router.get("/", response_model=list[CheckoutResponse])
 def get_checkouts( patron_id: int,  book_id: int | None = None, service: CheckoutService = Depends(get_checkout_service),):
     return service.get_checkout_list(  patron_id=patron_id,  book_id=book_id)
