e25e0abd8012d6af81596b304406ce4d9404def0
kohanyuk-a <kohanyuk.anastasia@lll.kpi.ua>
2025-12-16 17:36:09 +0200

    added patron update to patron_services.py

--- src/services/patron_services.py
+++ src/services/patron_services.py
@@ -1,9 +1,10 @@
 import re
+from typing import Optional
 from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException
 
 from src.repositories.patron_repository import PatronRepository
-from src.models import Patron, Notification
+from src.models import Patron, Notification, Checkout
 from src.templates import NotificationTemplates
 from src.send_email_notification import send_email_notification
 
@@ -73,4 +74,55 @@ class PatronService:
             raise HTTPException(
                 status_code=400,
                 detail="This patron already exists"
+            )
+
+    def update_patron(
+        self,
+        patron_id: int,
+        first_name: Optional[str] = None,
+        last_name: Optional[str] = None,
+        email: Optional[str] = None,
+        phone_number: Optional[str] = None
+    ):
+        patron = self.repo.get_by_id(patron_id)
+        if not patron:
+            raise HTTPException(status_code=404, detail="Patron not found.")
+
+        active_checkout = self.db.query(Checkout).filter(
+            Checkout.patron_id == patron_id,
+            Checkout.status.in_(["OK", "Soon", "Overdue"])
+        ).first()
+
+        if active_checkout:
+            if (first_name and first_name != patron.first_name) or \
+               (last_name and last_name != patron.last_name):
+                raise HTTPException(
+                    status_code=400,
+                    detail="Cannot change name with active checkouts."
+                )
+
+        if email:
+            email_pattern = r"^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+\.[A-Za-z]+$"
+            if not re.match(email_pattern, email):
+                raise HTTPException(status_code=400, detail="Incorrect email format")
+            patron.email = email
+
+        if phone_number:
+            if len(phone_number) != 10 or not phone_number.isdigit():
+                raise HTTPException(status_code=400, detail="Incorrect phone number format")
+            patron.phone_number = phone_number
+
+        if first_name:
+            patron.first_name = first_name
+        if last_name:
+            patron.last_name = last_name
+
+        try:
+            self.db.commit()
+            return {"message": "Updated successfully", "patron_id": patron.patron_id}
+        except IntegrityError:
+            self.db.rollback()
+            raise HTTPException(
+                status_code=400,
+                detail="Email or phone already exists"
             )
